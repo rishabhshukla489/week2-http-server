69165b376c30d4c3cf68f5c532253c15cdfcf2c1
Rishabh Shukla <rishabh.489.c@gmail.com>
2024-05-20 02:46:20 +0530

    added http server

--- index.js
+++ index.js
@@ -5,5 +5,12 @@ const app=express()
 app.get('/',(req,res)=>{
     res.send('hello world')
 })
+app.post('/api',(req,res)=>{
+    console.log(req.headers);
+    res.send({
+    "msg":"sdsdsd",
+    "sas": "sasa"
+    })
+})
 
 app.listen(3000)
